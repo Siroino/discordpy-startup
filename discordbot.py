c115e49daf1725db66567f653f1d56e32a876f73
Siroino <69386526+Siroino@users.noreply.github.com>
2020-08-09 14:47:49 +0900

    update_ver1.py

--- discordbot.py
+++ discordbot.py
@@ -28,32 +28,30 @@ async def ping(ctx):
 async def name(ctx):
     await ctx.send('Torisan')
 
+@bot.command()
+async def version(ctx):
+    await ctx.send('ver1')
     
-import random
+
 list = ["刀","扇", "薙", "銃", "忍", "傘", "書", "毒", "絡", "騎", "古", "琵", "炎", "笛", "戦", "社","経", "絆","機", "新","爪","拒", "鎌", "塵", "旗","橇","鏡","櫂","兜", "槌","嵐"]
-random.shuffle(list)
+
 
 @bot.command()
 import random
-list = ["刀","扇", "薙", "銃", "忍", "傘", "書", "毒", "絡", "騎", "古", "琵", "炎", "笛", "戦", "社","経", "絆","機", "新","爪","拒", "鎌", "塵", "旗","橇","鏡","櫂","兜", "槌","嵐"]
-random.shuffle(list)
+
 async def megami3(ctx):
-    random.shuffle(list)
+    await random.shuffle(list)
     await ctx.send(str(list[0]+list[1]+list[2])+"とかで良いんじゃないですか？")
     
 @bot.command()
-import random
-list = ["刀","扇", "薙", "銃", "忍", "傘", "書", "毒", "絡", "騎", "古", "琵", "炎", "笛", "戦", "社","経", "絆","機", "新","爪","拒", "鎌", "塵", "旗","橇","鏡","櫂","兜", "槌","嵐"]
-random.shuffle(list)
 async def megami2(ctx):
-    list = ["刀","扇", "薙", "銃", "忍", "傘", "書", "毒", "絡", "騎", "古", "琵", "炎", "笛", "戦", "社","経", "絆","機", "新","爪","拒", "鎌", "塵", "旗","橇","鏡","櫂","兜", "槌","嵐"]
-    random.shuffle(list)
+    await random.shuffle(list)
     await ctx.send(str(list[0]+list[1])+"とかで良いんじゃないですか？")
     
 @bot.event
 async def on_message(message):
     if message.content=="megami1":
-        megami = ["刀","扇", "薙", "銃", "忍", "傘", "書", "毒", "絡", "騎", "古", "琵", "炎", "笛", "戦", "社","経", "絆","機", "新","爪","拒", "鎌", "塵", "旗","橇","鏡","櫂","兜", "槌","嵐"]
+        megami = ["刀","扇", "薙", "銃", "忍", "傘", "書", "毒", "絡", "騎", "古", "琵琶", "炎", "笛", "戦", "社","経", "絆","機", "新","爪","拒", "鎌", "塵", "旗","橇","鏡","櫂","兜", "槌","嵐"]
         choice = random.choice(megami)
         await message.send_message(message.channel, choice) 
         
